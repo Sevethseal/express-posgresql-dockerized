d954a9ee9a8242c1cf7f56ba873517fac69a07ec
Nirmal Itty <nirmalitty@gmail.com>
2023-03-26 22:20:44 +0530

    Update migration file

--- scripts/migrate.js
+++ scripts/migrate.js
@@ -5,8 +5,7 @@ const db = require("../src/db");
     const isTable = await db.query(
       "SELECT EXISTS (SELECT FROM information_schema.tables WHERE table_schema LIKE 'public' AND table_type LIKE 'BASE TABLE' AND  table_name='book')"
     );
-    console.log(isTable, "tableValue");
-    if (isTable?.result?.rows[0]?.exists) {
+    if (isTable?.rows[0]?.exists) {
       await db.query("DROP TABLE book");
     }
     await db.query(
